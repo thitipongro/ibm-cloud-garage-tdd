fa59dea177ddaaa971a1c4362439e445f035ec4a
thitipongroo <thitipong@computerlogy.com>
2020-12-27 23:54:46 +0700

    Pass the third test

--- server/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
+++ server/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
@@ -13,7 +13,9 @@ describe('a fahrenheitToCelsius function should', () => {
   it('return 10 for 50', () => {
     expect(fahrenheitToCelsius(50)).toBe(10);
   });
-  it.todo('return 100 for 212');
+  it('return 100 for 212', () => {
+    expect(fahrenheitToCelsius(212)).toBe(100);
+  });
   it.todo('return -40 for -40');
   it.todo('return -273.15 for -459.67');
 });
