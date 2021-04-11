66b79b41448b3d6d812a55fc648172726fa1474b
thitipongroo <thitipong@computerlogy.com>
2021-04-11 11:39:39 +0700

    Add guard cluase statemment for non-strign

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -1,4 +1,5 @@
 let isPalindrome = (phrase) => {
+  if (typeof phrase !== 'string') throw new Error('input must be a string');
   if (phrase === '') throw new Error('empty strings are not palindrome');
   if (phrase.trim() === '') return false;
   return phrase === phrase.split('').reverse().join('')
