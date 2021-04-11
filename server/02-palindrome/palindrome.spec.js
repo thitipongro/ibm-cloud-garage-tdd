aeafd86f49f02e08339747297e4ac04cdbb02797
thitipongroo <thitipong@computerlogy.com>
2021-04-11 10:55:25 +0700

    introduce design for palindrome function

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -2,4 +2,11 @@ describe('the palindrome canary spec', () => {
   it('shows the infrastructure works', () => {
     expect(true).toBe(true);
   });
+  it.todo ('true for mom');
+  it.todo ('false for dude');
+  it.todo ('true for mom mom');
+  it.todo ('false for dad mom');
+  it.todo ('true for whitespace');
+  it.todo ('error for empty string');
+  it.todo ('error for not a string');
 });
