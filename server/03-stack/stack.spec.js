4fd212cc544d024a9981a1b63da9d6ef84e15d99
thitipongroo <thitipong@computerlogy.com>
2021-04-12 00:21:45 +0700

    stack size starts zero

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -1,5 +1,6 @@
 let stack = {
-  isEmpty: () => true
+  isEmpty: () => true,
+  size: () => 0
 };
 
 describe('the stack canary spec', () => {
@@ -12,7 +13,9 @@ describe('a stack', () => {
   it('starts empty', () => {
     expect(stack.isEmpty()).toBe(true);
   });
-  it.todo('starts with stack size of 0');
+  it('starts with stack size of 0', () => {
+    expect(stack.size()).toBe(0);
+  });
   it.todo('is not empty when pushed');
   it.todo('stack size is 1 when pushed');
   it.todo('stack is empty when pushed and popped');
