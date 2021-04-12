46b224a81f63c774fc292a6c03830bc5376d34d1
thitipongroo <thitipong@computerlogy.com>
2021-04-12 11:08:28 +0700

    stack is empty when pushed and pop

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -7,7 +7,8 @@ const stackFactory = () => {
     push: () => {
       empty = false;
       count += 1;
-    }
+    },
+    pop: () => { empty = true; }
   }
 };
 
@@ -37,7 +38,11 @@ describe('a stack', () => {
     stack.push();
     expect(stack.size()).toBe(1);
   });
-  it.todo('stack is empty when pushed and popped');
+  it('stack is empty when pushed and popped', () => {
+    stack.push();
+    stack.pop();
+    expect(stack.isEmpty()).toBe(true);
+  });
   it.todo('stack size is 0 when pushed and popped');
   it.todo('throws overflow error when pushing to a stack at full capacity');
   it.todo('throw underflow error when popping an empty stack');
