1227d11c9172e080f4a12fed72d938a6b3875856
thitipongroo <thitipong@computerlogy.com>
2021-04-12 17:24:10 +0700

    prime factor for 2

--- server/04-prime-factors/prime-factors.spec.js
+++ server/04-prime-factors/prime-factors.spec.js
@@ -1,4 +1,7 @@
-const findPrimeFactors = number => [];
+const findPrimeFactors = number => {
+  if (number === 1) return [];
+  return [number];
+};
 
 describe('the prime factors canary spec', () => {
   it('shows the infrastructure works', () => {
@@ -10,7 +13,9 @@ describe('a prime factors function should', () => {
   it('return none for 1', () => {
     expect(findPrimeFactors(1)).toEqual([]);
   });
-  it.todo('return 2 for 2');
+  it('return 2 for 2', () => {
+    expect(findPrimeFactors(2)).toEqual([2]);
+  });
   it.todo('return 3 for 3');
   it.todo('return 2, 2 for 4');
   it.todo('return 5 for 5');
