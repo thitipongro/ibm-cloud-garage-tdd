2a57528101d0c89910f95b1970261306c82e103c
thitipongroo <thitipong@computerlogy.com>
2021-04-12 15:55:46 +0700

    stack kata complete

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -1,11 +1,12 @@
-const stackFactory = () => {
+const stackFactory = (capacity = 2) => {
+  if (capacity < 1) throw new Error('invalid capacity');
   let elements = [];
 
   return {
     isEmpty: () => elements.length === 0,
     size: () => elements.length,
     push: (element) => {
-      if (elements.length === 2) throw new Error('capacity overflow error');
+      if (elements.length === capacity) throw new Error('capacity overflow error');
       elements.push(element);
     },
     pop: () => {
