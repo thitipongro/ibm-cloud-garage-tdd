459e5511e45ae10c2c29a7f8f97144aaff038a0d
thitipongroo <thitipong@computerlogy.com>
2021-04-13 01:57:33 +0700

    prime factors final version

--- server/04-prime-factors/prime-factors.spec.js
+++ server/04-prime-factors/prime-factors.spec.js
@@ -1,11 +1,7 @@
 const findPrimeFactors = number => {
   let factors = [];
-  if (number > 1) {
-    while (number % 2 === 0) {
-      factors.push(2);
-      number /= 2;
-    }
-    if (number > 1) factors.push(number);
+  for (let divisor = 2; number > 1; divisor++) {
+    for (; number % divisor === 0; number /= divisor) factors.push(divisor);
   };
   return factors;
 };
@@ -41,5 +37,7 @@ describe('a prime factors function should', () => {
   it('return 2, 2, 2 for 8', () => {
     expect(findPrimeFactors(8)).toEqual([2,2,2]);
   });
-  it.todo('return 3, 3 for 9');
+  it('return 3, 3 for 9', () => {
+    expect(findPrimeFactors(9)).toEqual([3,3]);
+  });
 });
